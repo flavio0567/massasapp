e6d7b74722cb45a3092cd66a875869886ce16632
Flavio Rocha <fmrocha@gmail.com>
2020-10-07 14:52:31 -0400

    feat. setup layout for all devices

--- src/shared/service/api.ts
+++ src/shared/service/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 // import config from '../../config/config.js';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
-  // baseURL: 'https://massasapi.massasdacecilia.com.br/',
+  // baseURL: 'http://localhost:3333',
+  baseURL: 'https://massasapi.massasdacecilia.com.br/',
 });
 
 export default api;
