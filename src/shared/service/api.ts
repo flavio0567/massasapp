fdf9587b73c8f0c3f89878fdcb8aa2e4e067bb33
Flavio Rocha <fmrocha@gmail.com>
2020-11-11 12:08:49 -0500

    feat. turned off agreement bottom

--- src/shared/service/api.ts
+++ src/shared/service/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333/',
-  // baseURL: 'https://massasapi.massasdacecilia.com.br/',
+  baseURL: 'https://massasapi.massasdacecilia.com.br/',
 });
 
 export default api;
